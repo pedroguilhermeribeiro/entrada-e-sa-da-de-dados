21000021e7b919805d45842727f8366db8b63e75
pedroguilhermeribeiro <pedro.guilherme.ribeiro@escola.pr.gov.br>
2025-09-03 20:13:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+//tipos e dados
+//tipo SRTRING: TEXTO. IDENTIFICADO PELO USO DAS ASPAS 
+//DUPLA
+let nome = "Pedro";
+
+//tipo NUMBER: se trata de número.
+
+let num = 10
+let num2 = "10";
+
+//tipo booleano: aceita dois valores como referencia e retorna
+//um dado com base na condição.
+
+//let ave = "pinguim";
+//if (ave = true) {
+    //alert("é uma ave.")
+//} else{
+   // alert("não é ave");
+//}
+
+alert("Olá seja bem vino ao meu site");
+alert("Precisamos confirmar alguns dados, ante de continuar");
+let seuNome = prompt("Digite seu nome por favor!");
+
+alert("Seja bem vindo " + seuNome + " ao meu site"); //usamos concatenação
+alert("Agora precisamos saber sua idade!");
+let idade = prompt("Por favor digite sua idade!");
+alert("Ja sei que você tem " + idade + "e agora preciso do seu e-mail!");
+let email = prompt("Digite seu email!");
+alert("seu email é" + email);
+
+console.log(nome);
+console.log(num);
+console.log(num2);
+console.log(seuNome);
+console.log(idade);
+console.log(email);
+
